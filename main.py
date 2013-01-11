7271954064a2d79c9ce9cff9186bb518396a7a8b
Rob Berkes <rob.berkes@gmail.com>
2013-01-11 09:52:05 -0600

    factorBrute finished

--- main.py
+++ main.py
@@ -1,3 +1,20 @@
 
 def factorBrute(A):
+	n=2
+	newmax=A
+	vals=[1,]
+	for n in range(2,newmax):
+		if A % n == 0:
+			vals.append(n)
+			newmax=int(A/n)
+
+	print vals
+	return vals
+
+def sumFactors(vals):
 	
+	return
+
+
+vals=factorBrute(90)
+sumFactors(vals)
