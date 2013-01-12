fdf5a3aaa869512e23a2e7fed822e96be5a05a9d
Rob Berkes <rob.berkes@gmail.com>
2013-01-12 11:03:32 +0000

    add count

--- main.py
+++ main.py
@@ -3,13 +3,15 @@ def factorBrute(A):
 	n=2
 	newmax=A
 	vals=[1,]
+	count=0
 	for n in range(2,newmax):
+		count+=1
 		if A % n == 0:
 			vals.append(n)
 			newmax=int(A/n)
 
 	print vals
-	return vals
+	return vals,count
 
 def sumFactors(vals):
 	factorSum=0
@@ -22,10 +24,10 @@ def sumFactors(vals):
 NUMBER=4096
 numArray=[7,6,28,100,8128,256,512,1024,33550336]
 for NUMBER in numArray:
-	vals=factorBrute(NUMBER)
+	vals,count=factorBrute(NUMBER)
 	if sumFactors(vals)==NUMBER:
-		print "Perfect number! Sum of "+str(NUMBER)+" is "+str(sumFactors(vals))
+		print "("+str(count)+") Perfect number! Sum of "+str(NUMBER)+" is "+str(sumFactors(vals))
 	elif sumFactors(vals)==1:
-		print "Prime number! "+str(NUMBER)+"'s factor sum is 1"
+		print "("+str(count)+") Prime number! "+str(NUMBER)+"'s factor sum is 1"
 	else:
-		print "non perfect, sum of "+str(NUMBER)+"'s factors is "+str(sumFactors(vals))
+		print "("+str(count)+") non perfect, sum of "+str(NUMBER)+"'s factors is "+str(sumFactors(vals))
