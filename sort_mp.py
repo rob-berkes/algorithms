549c65b41f64e52cb1f3a31ffdeeeeefdd8ecf39
Rob Berkes <rob.berkes@gmail.com>
2013-02-04 13:40:32 -0600

    SMP quicksort using Queues, test

--- sort_mp.py
+++ sort_mp.py
@@ -1,35 +1,37 @@
 from lib import sorting 
-from multiprocessing import Process,Pipe
+from multiprocessing import Process,Pipe,Queue
 import random
 import sys
 if __name__ == '__main__' :
 	sys.setrecursionlimit(2000)
 	n=12  #number partitions to break into
 #	SORTME=[9,4,3,5,8,11,234,22,2355,78,3,99,4,788,100,22,234,34442,1,55,666,4478,85,332,7712,2,4,777,5,]
-#	SORTME=[(random.randint(0,9999999),2,4) for x in range(1000000)]
-	OFILE=open("/home/ec2-user/hits.list","r")
+	SORTME=[random.randint(0,9999999) for x in range(100)]
+#	OFILE=open("/home/ec2-user/hits.list","r")
 #	OFILE=open("rand.list","r")
-	UNSORTLIST=[]
+#	UNSORTLIST=[]
 	print 'start reading list...'
-	for line in OFILE:
-	        line=line.strip().split(' # ')
-	        UNSORTLIST.append(line)
-	OFILE.close()
+#	for line in OFILE:
+#	        line=line.strip().split(' # ')
+#	        UNSORTLIST.append(line)
+#	OFILE.close()
 	print 'done reading list .... starting mulitple procs'
-
-	pconn,cconn=Pipe()
+	Q=Queue()
+#	pconn,cconn=Pipe()
 	lyst=[]
-	p=Process(target=sorting.QuickSortMPListArray,args=(UNSORTLIST,cconn,n))
+#	p=Process(target=sorting.QuickSortMPListArray,args=(UNSORTLIST,cconn,n))
+	p=Process(target=sorting.QuickSortSiMPle,args=(SORTME,Q,n))
 	p.start()
 	print 'main proc started'
 
-	lyst=pconn.recv()
+	#lyst=pconn.recv()
+	print 'starting out write'
+	print Q.get()	
 	print 'joining child procs'
 	p.join()
-	print 'starting out write'
-	OFILE=open("/home/ec2-user/hits.sorted","w")
+#	OFILE=open("/home/ec2-user/hits.sorted","w")
 #	OFILE=open("rand.sorted","w")
-	for a in lyst:
-		OFILE.write(str(a)+'\n')
-	OFILE.close()
+#	for a in lyst:
+#		OFILE.write(str(a)+'\n')
+#	OFILE.close()
 	print 'all done!'
