5d4acda1e28f8fc375196bc33e67a10d399ac662
Rob Berkes <rob.berkes@gmail.com>
2013-02-04 12:54:41 -0600

    new algo started for shared mem qs

--- lib/sorting.py
+++ lib/sorting.py
@@ -1,5 +1,5 @@
 import random
-from multiprocessing import Process,Manager,Pipe
+from multiprocessing import Process,Manager,Pipe,Queue
 def QuickSort(A):
         if len(A)==1:
                 return A
@@ -20,10 +20,38 @@ def QuickSort(A):
                         else:
                                 greater.append(int(A[val]))
         return QuickSort(lesser)+pv+QuickSort(greater)
-def QuickSortListArray(A):
+def QuickSortSiMPle(A,Q,SplitDepth):
+	exitQ=Queue()
+	totalQueue=Queue()
+	SplitDepth-=1
         if len(A)==1:
-                return A
+		Q.put(A)
+                return 
         elif len(A)==0:
+                return 
+        elif SplitDepth<=1:
+		sortedlist=QuickSort(A)
+		for z in sortedlist:
+			Q.put(z)
+		return 
+	else:
+                PivotIndex=random.randint(0,len(A)-1)
+                PivotValue=int(A[PivotIndex])
+                lesser=[]
+                greater=[]
+                pv=[]
+                pv.append(PivotValue)
+		
+		lesser=[x for x in A if x < PivotValue]
+		greater=[x for x in A if x > PivotValue]
+		equals=[x for x in A if x == PivotValue]
+		pL=Process(target=QuickSortSiMPle,args=(lesser,exitQ,SplitDepth))
+		for y in equals:
+			exitQ.put(y)
+		pR=Process(target=QuickSortSiMPle,args=(greater,exitQ,SplitDepth))
+        return 
+def QuickSortListArray(A):
+        if len(A)<=1:
                 return A
         else:
                 PivotIndex=random.randint(0,len(A)-1)
